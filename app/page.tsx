461656af11035b06e8fb50c68bb31db607be4ccb
MisterChief53 <soderol3@hotmail.com>
2024-04-03 13:18:48 -0600

    finished smalltitlecomponent

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import CameraStream from "@/app/cameraStream/page";
 import PublishComponent from "@/components/publishComponent";
 import NavBar from "@/components/navbar";
+import SmallTitleComponent from "@/components/smallTitleComponent";
 
 
 interface NavbarProps{
@@ -15,13 +16,14 @@ interface NavItem{
 
 const links: NavItem[] = [
   {text: "Dashboard", url: "/"},
-  {text: "Leaderboard", url: "/"}
+  {text: "Leaderboard", url: "/leaderboard"}
 ];
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <NavBar logo="/aiden_logo.png" links={links}/>
+      <SmallTitleComponent/>
       Hello
       <PublishComponent/>
     </main>

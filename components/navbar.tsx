249cbc23a78d46970654119e012be1bb6093328f
MisterChief53 <soderol3@hotmail.com>
2024-04-03 12:30:02 -0600

    navbar now works on desktop

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -15,17 +15,24 @@ export default function NavBar(props: NavbarProps){
     return (
         <div className="">
             <nav className="fixed top-0 left-0 w-full h-16 bg-white text-black px-4">
-                <div className="flex items-center">
+                {/* <div className="flex items-center">
                     <Image
                         src={props.logo}
                         className="h-9 w-auto mr-4"
                         width={200}
                         height={200}
                     />
-                </div>
+                </div> */}
                 <ul className="flex space-x-4">
+                    <Image
+                        src={props.logo}
+                        className="h-12 w-auto mr-4 pt-4"
+                        width={200}
+                        height={200}
+                        alt="aiden logo"
+                    />
                     {props.links.map((link) => (
-                        <li key={link.url} className="text-xl hover:underline">
+                        <li key={link.url} className="text-xl hover:underline pt-4">
                             <Link href={link.url}>{link.text}</Link>
                         </li>
                     ))}

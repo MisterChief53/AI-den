b168fd984a7b2ebc09a4936141a3653c70c9a065
MisterChief53 <soderol3@hotmail.com>
2024-04-03 21:12:27 -0600

    fixed height on status components by setting the text on a p and setting its height manually

--- components/rowStatComponent.tsx
+++ components/rowStatComponent.tsx
@@ -6,9 +6,9 @@ interface Stat{
 export default function RowStatComponent(props: Stat){
     return (
         <div className="relative items-center justify-center">
-            <div className="h-20 w-50 min-h-20 min-w-60 bg-cover bg-no-repeat rounded-2xl overflow-hidden items-center justify-center bg-green-800">
+            <div className="h-19 w-50 min-w-60 bg-cover bg-no-repeat rounded-2xl overflow-hidden items-center justify-center bg-green-800">
                 <div className="flex items-center justify-start h-full text-white">
-                    {props.text}
+                    <p className="h-10 pl-5 pt-2">{props.text}</p>
                 </div>
             </div>
         </div>
